c610d3a85f72c270c2c23421fe1ff1a4fd58223a
Benjamin Gray <bengray178@gmail.com>
2018-11-30 12:07:29 +1000

    Set lint rules

--- src/editorManager.ts
+++ src/editorManager.ts
@@ -1,4 +1,4 @@
-import { CompositeDisposable } from 'atom'
+import { CompositeDisposable, TextEditor } from 'atom'
 
 /**
  * Tracks editors to apply and remove custom behaviour as needed
@@ -6,8 +6,22 @@ import { CompositeDisposable } from 'atom'
 
 export class EditorManager {
   subscriptions: CompositeDisposable
+  watchedEditors: Set<TextEditor>
 
   constructor () {
     this.subscriptions = new CompositeDisposable()
+    this.subscriptions.add(
+      atom.workspace.observeTextEditors(editor => {
+        this.handleObservedEditor(editor)
+      }),
+    )
+    this.watchedEditors = new Set()
+  }
+
+  handleObservedEditor(editor: TextEditor): void {
+    if (this.watchedEditors.has(editor)) return
+    this.watchedEditors.add(editor)
+
+    console.log("hello world")
   }
 }
