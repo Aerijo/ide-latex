e8a272d290d61d98bc1bff391f5da79269be4db2
Benjamin Gray <bengray178@gmail.com>
2019-04-13 13:13:38 +1000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
 import * as child_process from "child_process"
 
-import { AutoLanguageClient } from "atom-languageclient"
+import { AutoLanguageClient, LanguageServerProcess } from "atom-languageclient"
 
 class LatexLanguageClient extends AutoLanguageClient {
-  getGrammarScopes () { return [ "text.tex.latex", "text.tex.biber", "text.tex.bibtex" ] }
+  getGrammarScopes () { return ["text.tex.latex", "text.tex.biber", "text.tex.bibtex"] }
   getLanguageName () { return "LaTeX" }
   getServerName () { return "latex-language-server" }
 
-  startServerProcess (projectPath: string) {
+  startServerProcess (_projectPath: string) {
     console.log("starting latex server...")
 
     const command = atom.config.get("ide-latex.serverPath")
-    const spawned = child_process.spawn(command, { cwd: projectPath })
+    const env = process.env
+
+    const spawned = child_process.spawn(command, env) as LanguageServerProcess
 
     spawned.stdout.setEncoding("utf8")
     spawned.stderr.setEncoding("utf8")
+
+    // spawned.stdin.on("data", data => {
+    //   console.log("IN:", data)
+    // })
+    // spawned.stdout.on("data", data => {
+    //   console.log("OUT:", data)
+    // })
 
     console.log("spawned", spawned)
 
@@ -23,6 +32,14 @@ class LatexLanguageClient extends AutoLanguageClient {
     })
 
     return spawned
+  }
+
+  preInitialization () {
+    console.log("pre init")
+  }
+
+  postInitialization () {
+    console.log("post init")
   }
 }
 
